ee660086396453ff96fc2cae43966afa5ff786ca
FransK <kouwenhoven.frans@gmail.com>
2024-11-16 21:10:42 -0800

    feat: read a csv file in the upload api

--- webserver/app.go
+++ webserver/app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"log"
 	"net/http"
 )
@@ -24,19 +25,23 @@ func (app App) Start() error {
 func (app App) uploadData(w http.ResponseWriter, r *http.Request) {
 	log.Println("Upload Data invoked.")
 
-	// file, _, err := r.FormFile("file")
-	// if err != nil {
-	// 	http.Error(w, "Unable to read file", http.StatusBadRequest)
-	// 	log.Printf("Error reading file: %v", err)
-	// 	return
-	// }
-	// defer file.Close()
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Unable to read file", http.StatusBadRequest)
+		log.Printf("Error reading file: %v", err)
+		return
+	}
+	defer file.Close()
 
-	// reader := csv.NewReader(file)
-	// rows, err := reader.ReadAll()
-	// if err != nil {
-	// 	http.Error(w, "Unable to parse CSV", http.StatusBadRequest)
-	// 	log.Printf("Error parsing CSV: %v", err)
-	// 	return
-	// }
+	reader := csv.NewReader(file)
+	rows, err := reader.ReadAll()
+	if err != nil {
+		http.Error(w, "Unable to parse CSV", http.StatusBadRequest)
+		log.Printf("Error parsing CSV: %v", err)
+		return
+	}
+
+	for _, record := range rows {
+		log.Println(record[0])
+	}
 }
