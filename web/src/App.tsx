66e8376ca543fb8be29ef31b4ffbd979d6581340
Frans Kouwenhoven <kouwenhoven.frans@gmail.com>
2024-11-23 21:38:36 -0800

    move test data

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import MyScatter from './components/ui/ScatterChart'
 import MyDropdown from './components/ui/SearchableDropdown'
 import { animals } from "./testdata/animals";
+import { data01, data02 } from "./testdata/chartdata";
 import { useState } from "react";
 
 function App() {
@@ -10,7 +11,10 @@ function App() {
 
   return (
     <>
-      <MyScatter />
+      <MyScatter 
+        data01={data01}
+        data02={data02}
+      />
       <MyDropdown
         options={animals}
         label="name"
