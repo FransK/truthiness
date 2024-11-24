4368cc76d74ee31451755dd90303c01cfb54f9e5
Frans Kouwenhoven <kouwenhoven.frans@gmail.com>
2024-11-24 10:26:57 -0800

    cleaning code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,12 @@
-
-import './App.css'
-import MyScatter from './components/ui/ScatterChart'
-import MyDropdown from './components/ui/SearchableDropdown'
-import { animals } from "./testdata/animals";
-import { data01, data02 } from "./testdata/chartdata";
-import { useState } from "react";
+import "./App.css";
+import ExperimentViewer from "./features/ExperimentViewer";
 
 function App() {
-  const [value, setValue] = useState("Select option...");
-
   return (
     <>
-      <MyScatter 
-        data01={data01}
-        data02={data02}
-      />
-      <MyDropdown
-        options={animals}
-        label="name"
-        id="id"
-        selectedVal={value}
-        handleChange={(val: string) => setValue(val)}
-      />
+      <ExperimentViewer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ web/src/features/ExperimentViewer.tsx
@@ -0,0 +1,27 @@
+import "./ExperimentViewer.css";
+
+import { useState } from "react";
+import MyScatter from "../components/ui/ScatterChart/ScatterChart";
+import MyDropdown from "../components/ui/SearchableDropdown/SearchableDropdown";
+import { animals } from "../testdata/animals";
+import { data01, data02 } from "../testdata/chartdata";
+
+export default function ExperimentViewer() {
+  const [experiment, setExperiment] = useState("Select experiment...");
+
+  return (
+    <div className="experiment-viewer">
+      <div className="experiment">
+        <h2>Select an experiment:</h2>
+        <MyDropdown
+          options={animals}
+          label="name"
+          id="id"
+          selectedVal={experiment}
+          handleChange={(exp: string) => setExperiment(exp)}
+        />
+      </div>
+      <MyScatter data01={data01} data02={data02} />
+    </div>
+  );
+}
